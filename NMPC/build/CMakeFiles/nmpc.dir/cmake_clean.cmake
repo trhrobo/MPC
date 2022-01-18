ca19507136aae4cf97e18fc08dbec896104b0158
TsuruharaKota <m1830@s.akashi.ac.jp>
2022-01-18 15:32:52 +0900

    completegit add .!

---
+++ NMPC/build/CMakeFiles/nmpc.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/nmpc.dir/nmpc.cpp.o"
+  "nmpc.pdb"
+  "nmpc"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/nmpc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- build/CMakeFiles/NMPC.dir/cmake_clean.cmake
+++ build/CMakeFiles/NMPC.dir/cmake_clean.cmake
@@ -1,10 +1,5 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/NMPC.dir/NMPC/mpc.cpp.o"
-  "NMPC.pdb"
-  "NMPC"
-)
 
 # Per-language clean rules from dependency scanning.
-foreach(lang CXX)
+foreach(lang )
   include(CMakeFiles/NMPC.dir/cmake_clean_${lang}.cmake OPTIONAL)
 endforeach()
